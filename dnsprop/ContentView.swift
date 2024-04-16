64d7a14c797bd6fe2fa9b7d2662ff796668fd33f
Mike Neal <mike@bluepixel.dev>
2024-04-16 19:45:52 -0400

    update front-end UI

--- dnsprop/ContentView.swift
+++ dnsprop/ContentView.swift
@@ -11,16 +11,25 @@ struct ContentView: View {
 	@State private var url = ""
     var body: some View {
 		Form {
+			HStack {
 			TextField("URL", text: $url)
-			
-			VStack {
-				locationView(dataCenter: "US-West", status: true, firstIP: "10.0.0.1", secondIP: "10.0.0.2")
-				locationView(dataCenter: "US-East", status: true, firstIP: "10.0.0.1", secondIP: "10.0.0.2")
-				locationView(dataCenter: "Canada-West", status: false, firstIP: "10.0.0.1")
-				locationView(dataCenter: "Canada-East", status: true, firstIP: "10.0.0.1")
-				
+				.padding()
+				.textFieldStyle(.roundedBorder)
+				Button("Submit") {
+					//Do stuff
+				}
 			}
+			.padding()
 		}
+		VStack {
+			locationView(dataCenter: "US-West", status: true, firstIP: "10.0.0.1", secondIP: "10.0.0.2")
+			locationView(dataCenter: "US-East", status: true, firstIP: "10.0.0.1", secondIP: "10.0.0.2")
+			locationView(dataCenter: "Canada-West", status: false, firstIP: "10.0.0.1")
+			locationView(dataCenter: "Canada-East", status: true, firstIP: "10.0.0.1")
+			
+		}
+		.padding()
+		Spacer()
     }
 }
 
--- dnsprop/locationViews.swift
+++ dnsprop/locationViews.swift
@@ -39,7 +39,7 @@ struct locationView: View {
 				}
 			}
 		}
-		.padding()
+		.padding(5)
     }
 }
 
