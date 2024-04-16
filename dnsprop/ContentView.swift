cf743f11c3a11cb8cf4af1d4ea3983afd60e364f
Mike Neal <mike@bluepixel.dev>
2024-04-16 19:32:20 -0400

    set up basic UI

--- dnsprop/ContentView.swift
+++ dnsprop/ContentView.swift
@@ -8,14 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+	@State private var url = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+		Form {
+			TextField("URL", text: $url)
+			
+			VStack {
+				locationView(dataCenter: "US-West", status: true, firstIP: "10.0.0.1", secondIP: "10.0.0.2")
+				locationView(dataCenter: "US-East", status: true, firstIP: "10.0.0.1", secondIP: "10.0.0.2")
+				locationView(dataCenter: "Canada-West", status: false, firstIP: "10.0.0.1")
+				locationView(dataCenter: "Canada-East", status: true, firstIP: "10.0.0.1")
+				
+			}
+		}
     }
 }
 
---
+++ dnsprop/locationViews.swift
@@ -0,0 +1,48 @@
+//
+//  locationViews.swift
+//  dnsprop
+//
+//  Created by Michael Neal on 4/16/24.
+//
+
+import SwiftUI
+
+struct locationView: View {
+	var dataCenter: String
+	var status: Bool
+	var firstIP: String
+	var secondIP: String?
+	var body: some View {
+		HStack {
+			Text("\(dataCenter):")
+			Spacer()
+			if !status {
+				Image(systemName: "network.slash")
+					.foregroundColor(.red)
+			} else {
+				VStack {
+					HStack {
+						Spacer()
+						Text(firstIP)
+						Image(systemName: "network")
+							.foregroundColor(.green)
+					}
+					if secondIP != nil {
+						HStack {
+							Spacer()
+							Text(secondIP!)
+							Image(systemName: "network")
+								.foregroundColor(.green)
+						}
+						
+					}
+				}
+			}
+		}
+		.padding()
+    }
+}
+
+#Preview {
+	locationView(dataCenter: "US-West", status: true, firstIP: "10.0.0.1", secondIP: "10.0.0.2")
+}
