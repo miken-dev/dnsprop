3b76567e02de1de5b257e846ba1697b1ef118716
Mike Neal <mike@bluepixel.dev>
2024-04-16 19:10:32 -0400

    Initial Commit

---
+++ dnsprop/dnspropApp.swift
@@ -0,0 +1,17 @@
+//
+//  dnspropApp.swift
+//  dnsprop
+//
+//  Created by Michael Neal on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct dnspropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
